0bd0d6950e54eac531a817df23b13f873c4c5d49
Ruben Rosales <rrpaton@hotmail.es>
2015-09-15 14:13:59 +0200

    Changes on MF_OTP and LabelTypeTLV

---
+++ src/es/tid/ospf/ospfv2/lsa/tlv/subtlv/LabelTypeTLV.java
@@ -0,0 +1,91 @@
+package es.tid.ospf.ospfv2.lsa.tlv.subtlv;
+
+import es.tid.bgp.bgp4.update.tlv.complexFields.BitmapLabelSet;
+
+
+public class LabelTypeTLV  extends OSPFSubTLV {
+
+	
+	private int priority;
+	private long reserved;
+	private BitmapLabelSet BitMapLS;
+	
+	
+	public LabelTypeTLV(){
+		super();
+		this.setTLVType(OSPFSubTLVTypes.LabelTypeTLV);		
+	}
+	
+	
+	public LabelTypeTLV(byte[] bytes, int offset) {
+		super(bytes,offset);
+		decode();
+	}
+	
+
+	
+	public void encode(){	
+		
+		int valueLength = 4;
+		
+		
+		if (BitMapLS != null){
+		
+			BitMapLS.encode();
+			valueLength += BitMapLS.getLength();
+		}
+		this.setTLVValueLength(valueLength);
+		
+		this.tlv_bytes = new byte[this.getTotalTLVLength()];
+		
+		int offset = 4;
+		
+		tlv_bytes[offset]=(byte)(priority&0xFF);
+		
+		offset = offset + 1;
+		
+		tlv_bytes[offset]=(byte)((reserved>>16) & 0xFF);
+		
+		offset = offset + 1;
+		
+		tlv_bytes[offset]=(byte)((reserved>>8) & 0xFF);
+		
+		offset = offset + 1;
+		
+		tlv_bytes[offset]=(byte)(reserved & 0xFF);
+		
+		offset = offset + 1;
+		
+		if (BitMapLS != null)
+			System.arraycopy(this.BitMapLS.getBytes(),0 , this.tlv_bytes,offset,BitMapLS.getLength() );	
+	}
+	
+
+	
+	public void decode() {
+		
+		int offset=4;
+		
+		priority=((tlv_bytes[offset]&0xFF));
+		
+		offset = offset + 4;
+		
+		this.BitMapLS = new BitmapLabelSet (tlv_bytes,offset);
+		
+	}
+	
+
+	
+
+
+	public String toString(){
+		
+		String str =  "[LabelTypeTLV " + "\n Priority: " + priority + "\n Reserved: " + reserved + "\n BitMapLS: " + BitMapLS.toString();
+		str+=">";
+		return str;
+	
+	}
+		
+	
+	
+}
